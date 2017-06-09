464e2c8791d6a4a29da1f1f002752d7aab51ca74
sajidhz1 <sajidhzazahir@gmail.com>
2017-06-09 22:09:09 +0530

    First Commit

---
+++ Client/Aiesec-App/Aiesec_App.UWP/MainPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Aiesec_App.UWP
+{
+    public sealed partial class MainPage
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            LoadApplication(new Aiesec_App.App());
+        }
+    }
+}
---
+++ Client/Aiesec-App/Aiesec_App/Views/AboutPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿
+using Xamarin.Forms;
+
+namespace Aiesec_App.Views
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
