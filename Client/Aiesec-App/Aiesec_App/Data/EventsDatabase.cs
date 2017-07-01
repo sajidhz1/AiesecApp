35b1b24dc199fa8758e02b386d1d51324fa4f285
sajidhz1 <sajidhzazahir@gmail.com>
2017-07-01 22:44:38 +0530

    Added Events DataBase Connection

---
+++ Client/Aiesec-App/Aiesec_App/Data/EventsDatabase.cs
@@ -0,0 +1,27 @@
+﻿using Aiesec_App.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aiesec_App.Data
+{
+    class EventsDatabase : IDataBase<EventItem>
+    {
+        public Task<int> DeleteItemAsync(EventItem item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<EventItem>> GetItemsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> SaveItemAsync(EventItem item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Client/Aiesec-App/Aiesec_App/Data/EventsManager.cs
@@ -0,0 +1,34 @@
+﻿using Aiesec_App.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aiesec_App.Data
+{
+    class EventsManager
+    {
+        IRestService<EventItem> restService;
+
+        public EventsManager(IRestService<EventItem> service)
+        {
+            restService = service;
+        }
+
+        public Task<List<EventItem>> GetItemsAsync()
+        {
+            return restService.RefreshDataAsync();
+        }
+
+        public Task SaveTaskAsync(EventItem item, bool isNewItem = false)
+        {
+            return restService.SaveItemAsync(item, isNewItem);
+        }
+
+        public Task DeleteTaskAsync(EventItem item)
+        {
+            return restService.DeleteItemAsync(item.ID);
+        }
+    }
+}
