b3720434bbd730fabd38c29ec49e3221b375dea2
Dulitha Ruvin Daluwatta <dulitha.ruvin@gmail.com>
2017-07-08 12:06:33 +0530

    JWT token based authentication added using passport.js and expressjwt

--- Server/AiesecAppBackEnd/api/models/User.js
+++ Server/AiesecAppBackEnd/api/models/User.js
@@ -7,8 +7,74 @@
 
 module.exports = {
 
-  attributes: {
+  schema: true,
+  autoCreatedAt: false,
+  autoUpdatedAt: false,
+  tableName: 'user',
+  identity: 'User',
 
+  attributes: {
+    idUser: {
+      type: 'integer',
+      primaryKey: true,
+      unique: true,
+      required: true
+    },
+
+    email: {
+      type: 'email',
+      required: true,
+      unique: true
+    },
+
+    username: {
+      type: 'string',
+      required: true,
+      unique: true,
+      alphanumericdashed: true
+    },
+
+    password: {
+      type: 'string',
+      required: true
+    },
+
+    approved: {
+      type: 'integer',
+      required: true,
+      defaultsTo: 0
+    },
+
+    userType: {
+      type: 'integer',
+      required: true
+    },
+
+    expired: {
+      type: 'integer',
+      required: true,
+      defaultsTo: 0
+    },
+
+    LocalCommitte_idLocalCommitte:{
+      model: 'localcommitte'
+    },
+
+    toJSON: function () {
+      var obj = this.toObject();
+      delete obj.password;
+      return obj;
+    }
+  },
+
+  //one of the nice things about sails is that it makes our services available globally.
+  beforeUpdate: function (values, next) {
+    CipherService.hashPassword(values);
+    next();
+  },
+  beforeCreate: function (values, next) {
+    CipherService.hashPassword(values);
+    next();
   }
 };
 
