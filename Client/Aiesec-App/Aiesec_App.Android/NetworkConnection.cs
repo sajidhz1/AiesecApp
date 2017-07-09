5bdbe5d889bb4bc55900e020679d3f2e318a6408
sajidhz1 <sajidhzazahir@gmail.com>
2017-07-09 23:39:10 +0530

    Network Connection Check Added

---
+++ Client/Aiesec-App/Aiesec_App.Android/NetworkConnection.cs
@@ -0,0 +1,30 @@
+﻿using Xamarin.Forms;
+using Aiesec_App.Droid;
+using Aiesec_App.Services;
+using System;
+using Android.Net;
+using Android.Content;
+using Application = Android.App.Application;
+
+[assembly: Dependency(typeof(NetworkConnection))]
+namespace Aiesec_App.Droid
+{
+    public class NetworkConnection : INetworkConnection
+    {
+        public bool IsConnected { get; set; }
+
+        public void CheckNetworkConnection()
+        {
+            var connectivityManager = (ConnectivityManager)Application.Context.GetSystemService(Context.ConnectivityService);
+            var activeNetworkInfo = connectivityManager.ActiveNetworkInfo;
+            if (activeNetworkInfo != null && activeNetworkInfo.IsConnectedOrConnecting)
+            {
+                IsConnected = true;
+            }
+            else
+            {
+                IsConnected = false;
+            }
+        }
+    }
+}
