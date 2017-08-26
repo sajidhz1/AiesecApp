0471548c6411cbabfb4b86c46dc7e2fecb852887
Dulitha Ruvin Daluwatta <dulitha.ruvin@gmail.com>
2017-08-26 15:46:12 +0530

    Complain model bug fixed and proc file added

--- Server/AiesecAppBackEnd/api/models/Complain.js
+++ Server/AiesecAppBackEnd/api/models/Complain.js
@@ -13,43 +13,40 @@ module.exports = {
   identity: 'Complain',
 
   attributes: {
-
     idComplain: {
-      idComplain: {
-        type: 'integer',
-        primaryKey: true,
-        unique: true,
-        autoIncrement: true
-      },
+      type: 'integer',
+      primaryKey: true,
+      unique: true,
+      autoIncrement: true
+    },
 
-      ExchangeParticipant_idExchangeParticipant: {
-        model: 'ExchangeParticipant',
-        required: true
-      },
+    ExchangeParticipant_idExchangeParticipant: {
+      model: 'ExchangeParticipant',
+      required: true
+    },
 
-      Project_idProject: {
-        model: 'Project',
-        required: true
-      },
+    Project_idProject: {
+      model: 'Project',
+      required: true
+    },
 
-      description: {
-        type: 'string',
-        required: true
-      },
+    description: {
+      type: 'string',
+      required: true
+    },
 
-      createdDate: {
-        type: 'datetime'
-      },
+    createdDate: {
+      type: 'datetime'
+    },
 
-      updatedDate: {
-        type: 'datetime'
-      },
+    updatedDate: {
+      type: 'datetime'
+    },
 
-      complainStatus: {
-        type: 'boolean',
-        required: true,
-        defaultsTo: false
-      }
+    complainStatus: {
+      type: 'boolean',
+      required: true,
+      defaultsTo: false
     }
   }
 };
